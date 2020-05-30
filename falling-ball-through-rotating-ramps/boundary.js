22ce507efaf16e06226065b380353b7637a0ca14
gandolh <gusatualex@yahoo.com>
2020-05-31 01:23:32 +0300

    Add files via upload

---
+++ falling-ball-through-rotating-ramps/boundary.js
@@ -0,0 +1,35 @@
+
+class Boundary {
+    constructor(x, y, w, h){
+        this.w = w;
+        this.h = h;
+        let options={
+            friction:0.3,
+            restitution: 0.6,
+            isStatic: true,
+            
+        }
+        this.body = Bodies.rectangle(x, y, w, h,options);
+        //console.log(this.body);
+        World.add(world, this.body);
+        this.angularVel= random(-0.05,-0.01) || (0.01,0.05);
+        console.log(this.angularVel);
+    }
+    rotate(){
+    let angle=this.body.angle +this.angularVel;
+    Body.setAngle(this.body,angle);
+    }
+    show() {
+
+        let pos = this.body.position;
+        let angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill(170);
+        noStroke();
+        rect(0, 0, this.w, this.h);
+        pop();
+    }
+}
---
+++ falling-ball-through-rotating-ramps/box.js
@@ -0,0 +1,23 @@
+
+class Circle {
+    constructor(x, y, r){
+        this.r=r;
+        let options={
+            friction:0.3,
+            restitution: 0.6
+        }
+        this.body = Bodies.circle(x, y, r,options);
+        World.add(world, this.body);
+    }
+    show() {
+
+        let pos = this.body.position;
+       // let angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        //rotate(angle);
+        ellipseMode(CENTER);
+        ellipse(0,0,this.r,this.r);
+        pop();
+    }
+}
