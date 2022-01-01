6c153d2b8acf43c12d1ed53ee96e2cf2d845db49
gandolh <gusatualex@yahoo.com>
2022-01-01 23:30:18 +0200

    smoothing and bitwise operations

---
+++ smoothing.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('Resources/Photos/cats.jpg')
+
+cv.imshow('cats',img)
+
+#averaging
+average= cv.blur(img,(3,3))
+cv.imshow('average blur',average)
+
+#gaussianBlur
+gauss= cv.GaussianBlur(img,(3,3),sigmaX=0)
+cv.imshow('gaussian blur',gauss)
+
+#median blur
+median = cv.medianBlur(img,3)
+cv.imshow('median blur',median)
+
+#Bilateral blur
+bilateral = cv.bilateralFilter(img,5,15,15)
+cv.imshow('bilateral filter',bilateral)
+
+
+cv.waitKey(0)
