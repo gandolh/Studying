77bf8ffa88d4ac78f4d7aa6ec81974e872852b40
gandolh <gusatualex@yahoo.com>
2022-03-16 22:22:53 +0200

    OOHHHH THATS HOW  YOU DO LAYERS IN NEURAL NETWORKS

---
+++ digit_recognition/main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.python.keras.activations import softmax
+from tensorflow.python.keras.metrics import accuracy
+from tensorflow.python.keras import activations
+import cv2 as cv
+
+
+data = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = data.load_data()
+
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape= (28,28)))
+model.add(tf.keras.layers.Dense(units=128,activation = tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=128,activation = tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=10,activation = tf.nn.softmax))
+
+model.compile(optimizer ='adam', loss= 'sparse_categorical_crossentropy', metrics= ['accuracy'])
+model.fit(x_train, y_train,epochs = 3)
+loss, accuracy = model.evaluate(x_test,y_test)
+print(accuracy)
+print(loss)
