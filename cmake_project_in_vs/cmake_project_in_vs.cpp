54a4bd5eabd5d5c4080cdebba462333794a29f88
Gandolh <alex.gusatu@mail.centruldecalcul.ro>
2022-06-09 01:54:50 +0300

    it works?

---
+++ cmake_project_in_vs/cmake_project_in_vs.cpp
@@ -0,0 +1,12 @@
+﻿// cmake_project_in_vs.cpp : Defines the entry point for the application.
+//
+
+#include "cmake_project_in_vs.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
