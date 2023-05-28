7b2bcd188a5730a728ee0f333e56f6023d505b91
Gandolh <29356778+gandolh@users.noreply.github.com>
2023-05-28 15:38:43 +0300

    001 rust

---
+++ Rust/001/main.rs
@@ -0,0 +1,15 @@
+fn main()
+{
+    //getting started
+    // let x : u32 = 4;
+    // println!("Hello, world!, {}", x);
+    
+    //crashing rust
+    // let address = 0x012345usize;
+    // let r = address as *const i32;
+    // unsafe{
+    //     println!("r is: {}", *r);
+    // }
+
+    return;
+    }
